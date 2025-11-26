91f4802b16087b87a797110f1340f221fcd83a9d
Justis R <justis.root@gmail.com>
2025-11-26 04:31:50 +0000

    Fix join messages on latest versions and ensure they always send last

--- src/main/java/com/builtbybit/PlayerListener.java
+++ src/main/java/com/builtbybit/PlayerListener.java
@@ -4,11 +4,13 @@
 */
 package com.builtbybit;
 
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.server.ServerListPingEvent;
 
@@ -19,16 +21,21 @@ public class PlayerListener implements Listener {
 		e.setMotd(ChatColor.AQUA + "BuiltByBit " + ChatColor.GRAY + "Resource Test Server \n" + ChatColor.GREEN + "Powered by PebbleHost");
 	}
    
-    @EventHandler
+    @EventHandler (priority = EventPriority.MONITOR)
 	public void onJoin(PlayerJoinEvent e) {
 		e.getPlayer().setOp(true);
-		new JsonMessage().append(ChatColor.AQUA + "[BUILTBYBIT]").setClickAsURL("https://builtbybit.com").save()
-		.append(ChatColor.GRAY + " This is a BuiltByBit resource test server.").save()
-		.send(e.getPlayer());
-		e.getPlayer().sendMessage(ChatColor.GRAY + "You've been granted operator permissions.");
-		new JsonMessage().append(ChatColor.GRAY + "Hosting is provided in proud partnership with ").save()
-		.append(ChatColor.GREEN + "PebbleHost").setClickAsURL("https://pebblehost.com").save()
-		.send(e.getPlayer());
+
+		BaseComponent[] msg = new ComponentBuilder()
+			.append("[BUILTBYBIT]").color(net.md_5.bungee.api.ChatColor.AQUA).event(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://builtbybit.com"))
+			.append(" This is a BuiltByBit resource test server.").reset().color(net.md_5.bungee.api.ChatColor.GRAY).create();
+		e.getPlayer().spigot().sendMessage(msg);
+
+		e.getPlayer().sendMessage(ChatColor.GRAY + "You have been granted operator permissions.");
+
+		msg = new ComponentBuilder()
+			.append("Hosting is provided in proud partnership with ").color(net.md_5.bungee.api.ChatColor.GRAY)
+			.append("PebbleHost").color(net.md_5.bungee.api.ChatColor.GREEN).event(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://pebblehost.com")).create();
+		e.getPlayer().spigot().sendMessage(msg);
 	}
 
 }
